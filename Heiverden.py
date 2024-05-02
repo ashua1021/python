7ab4147c835ca986837bf87b2523869f2be8c2ad
ashua1021 <168414919+ashua1021@users.noreply.github.com>
2024-05-02 11:23:06 +0200

    Initial commit

---
+++ Heiverden.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 12 14:47:49 2023
+
+@author: ashua007
+"""
+
+
+#oppretter to tallvariabel
+A = 5
+B = 9
+ 
+print("Tallet i variabel A er ",A)
+print("Tallet i variabel B er ",B)
+sum=A+B
+print("summen av de to variabelene A+B er ",sum)
+
+#Finne differansen av de to tallvariabel
+diff=B-A
+
+print("differansen av de to variabelene A-B er ",diff)
+
+#gange de to tallverbalene sammen
+gange=A*B
+print("produktet av de to veriabelene A*B er " ,gange)
+
+
+        
+    
+            
+            
+            
+            
